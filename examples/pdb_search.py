14d5a39dcb8bbc4cd3d26242e6fc43c6b38c7bf5
Martin Stroet <martin.stroet@gmail.com>
2023-08-04 11:38:32 +1000

    Add example pdb searching

---
+++ examples/pdb_search.py
@@ -0,0 +1,26 @@
+import pathlib
+from atb_api_public.atb_api import API
+
+PDB_FILES = pathlib.Path("./example_pdbs")
+
+api = API(api_token='<ATB API TOKEN>', api_format='yaml', debug=True, timeout=60)
+
+matches = []
+missing = []
+for pdb_file in PDB_FILES.glob("*.pdb"):
+    print(f"Searching for: {pdb_file.name}")
+    with open(pdb_file) as fh:
+        response = api.Molecules.structure_search(structure_format='pdb', structure=fh.read(), netcharge='*')
+        if len(response["matches"]) == 0:
+            print("Molecule not found")
+            missing.append(pdb_file.name)
+        else:
+            print("Molecule(s) found: {0}".format(", ".join([f"molid={match['molid']}" for match in response["matches"]])))
+            matches.append(", ".join([pdb_file.name] + [str(match['molid']) for match in response['matches']]))
+    print()
+
+print("Matches")
+print("\n".join(matches))
+print()
+print("Missing")
+print("\n".join(missing))
